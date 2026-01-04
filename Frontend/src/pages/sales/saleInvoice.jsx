f4df505d419a6d14ba3b45f31c243d4e63f618b7
haroon-90 <haroonboy90@gmail.com>
2026-01-04 17:12:32 +0500

    39

--- Frontend/src/pages/sales/saleInvoice.jsx
+++ Frontend/src/pages/sales/saleInvoice.jsx
@@ -146,7 +146,7 @@ const SaleInvoice = ({ selected, handleClose, type, invoiceRef, handleDelete }) 
                     </div>
                 </div>
 
-                <div className="flex justify-center gap-3 px-6 py-4 border-t bg-gray-50">
+                <div className="flex justify-center gap-3 px-6 py-4 min-w-[210mm] border-t bg-gray-50">
                     <button
                         onClick={handlePrint}
                         className="px-5 py-2 flex items-center gap-2 rounded-lg bg-green-100 text-green-600 hover:bg-green-500 hover:text-white transition"
